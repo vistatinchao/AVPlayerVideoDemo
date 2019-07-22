17d1e693a64140e7a14fa455d537cf3397a9c09e
vistatinchao <929432087@qq.com>
2019-07-22 09:41:45 +0800

    Initial Commit

---
+++ AVPlayerVideoDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AVPlayerVideoDemo
+//
+//  Created by zouchao on 2019/7/22.
+//  Copyright © 2019 zouchao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
