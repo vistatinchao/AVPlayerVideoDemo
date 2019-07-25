47080cf8f5f41d8e29f732ac13a4ababb44a0210
vistatinchao <929432087@qq.com>
2019-07-25 23:30:38 +0800

    添加masonry
    1.添加masonry框架
    2.初始化播放器

--- AVPlayerVideoDemo/PlayViedo/PlayVideoView.h
+++ AVPlayerVideoDemo/PlayViedo/PlayVideoView.h
@@ -12,6 +12,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PlayVideoView : UIView
 
+/**播放链接*/
+@property (nonatomic ,copy) NSString *url;
+
++ (instancetype)playVideoViewWithUrl:(NSString *)url;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- AVPlayerVideoDemo/PlayViedo/PlayVideoView.m
+++ AVPlayerVideoDemo/PlayViedo/PlayVideoView.m
@@ -5,17 +5,64 @@
 //  Created by zouchao on 2019/7/22.
 //  Copyright © 2019 zouchao. All rights reserved.
 //
-
+#define MAS_SHORTHAND
 #import "PlayVideoView.h"
+#import "Masonry.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface PlayVideoView()
+
+/**playLayer*/
+@property (nonatomic,strong) AVPlayerLayer *playLayer;
+/**播放player*/
+@property (nonatomic,strong) AVPlayer *player;
+/**playItem*/
+@property (nonatomic,strong) AVPlayerItem *playItem;
+@end
 
 @implementation PlayVideoView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
++ (instancetype)playVideoViewWithUrl:(NSString *)url {
+    PlayVideoView *videoView = [[self alloc]init];
+    videoView.url = url;
+    return videoView;
 }
-*/
 
+- (instancetype)init {
+    if (self = [super init]) {
+        [self setPlayUI];
+        [self setPlayVideoSubview];
+    }
+    return self;
+}
+
+- (void)setPlayUI {
+    NSURL *playUrl = [NSURL URLWithString:self.url];
+//    AVPlayerItem *playItem = [AVPlayerItem playerItemWithURL:playUrl];
+//    self.playItem = playItem;
+
+    AVPlayer *player = [AVPlayer playerWithURL:playUrl];
+    AVPlayerLayer *playLayer = [AVPlayerLayer playerLayerWithPlayer:player];
+    [self.layer addSublayer:playLayer];
+
+    self.playLayer = playLayer;
+    self.player = player;
+    // 开始播放
+    [self.player play];
+}
+
+- (void)setPlayVideoSubview {
+    UIView *coverView = [[UIView alloc]init];
+    [self addSubview:coverView];
+    
+}
+
+- (void)setUrl:(NSString *)url {
+    _url = url;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+     self.playLayer.frame = self.bounds;
+}
 @end
